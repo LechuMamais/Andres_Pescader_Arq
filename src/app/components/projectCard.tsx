ee2b5bd5272e891b02cfb9c96501b597de480486
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-05-06 21:47:40 +0200

    FEAT: add animation to projectCard

---
+++ src/app/components/projectCard.tsx
@@ -0,0 +1,86 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { Route } from 'next'
+import { Proyecto } from '../types'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useState, useEffect } from 'react'
+
+interface Props {
+  proyecto: Proyecto
+  key: string
+  index: number
+}
+
+export default function ProjectCard({ proyecto, key, index }: Props) {
+  const [isHovered, setIsHovered] = useState(false)
+  const [isMobileOrTablet, setIsMobileOrTablet] = useState(false)
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobileOrTablet(window.innerWidth < 1024) // Tailwind's `lg` breakpoint
+    }
+
+    checkIsMobile()
+    window.addEventListener('resize', checkIsMobile)
+
+    return () => {
+      window.removeEventListener('resize', checkIsMobile)
+    }
+  }, [])
+
+  return (
+    <Link
+      href={`/${proyecto.proy_id}` as Route}
+      key={key}
+      className='flex flex-col items-center h-[350px] relative'
+    >
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: index * 0.15, // animación secuencial
+          duration: 0.5,
+          ease: 'easeOut'
+        }}
+        className='w-full h-full relative overflow-hidden'
+        onHoverStart={() => setIsHovered(true)}
+        onHoverEnd={() => setIsHovered(false)}
+      >
+        <Image
+          className='w-full h-full object-cover transition duration-300'
+          style={{
+            filter: isHovered ? 'blur(4px) brightness(0.5)' : 'none',
+            scale: isHovered ? 1.01 : 1
+          }}
+          src={proyecto.imagen?.asset?.url || '/placeholder.jpg'}
+          alt={proyecto.titulo}
+          width={900}
+          height={600}
+        />
+        {isMobileOrTablet && (
+          <div className='absolute bottom-0 left-0 w-full h-16 z-10 flex items-center justify-center backdrop-blur-[4px] bg-[rgba(0,0,0,0.25)]'>
+            <h3 className='text-2xl text-white font-semibold text-center px-4 tracking-wide z-20'>
+              {proyecto.titulo}
+            </h3>
+          </div>
+        )}
+
+        <AnimatePresence>
+          {isHovered && !isMobileOrTablet && (
+            <motion.h3
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 20 }}
+              transition={{ duration: 0.3, ease: 'easeOut' }}
+              className='absolute bottom-1/2 left-1/2 transform -translate-x-1/2 translate-y-1/2 text-2xl text-white font-semibold text-center px-4 tracking-wide z-20'
+            >
+              {proyecto.titulo}
+            </motion.h3>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </Link>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import ProjectCard from './components/project_card'
+import ProjectCard from './components/projectCard'
 import { PROYECTOS_QUERY } from './sanity/apiGroks'
-
 import { client } from './sanity/client'
 import { Proyecto } from './types'
 
@@ -8,10 +7,11 @@ const options = { next: { revalidate: 30 } }
 
 export default async function Home() {
   const proyectos = await client.fetch<Proyecto[]>(PROYECTOS_QUERY, {}, options)
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-4 w-full'>
-      {proyectos.map(proyecto => (
-        <ProjectCard proyecto={proyecto} key={proyecto.proy_id} />
+      {proyectos.map((proyecto, index) => (
+        <ProjectCard proyecto={proyecto} key={proyecto.proy_id} index={index} />
       ))}
     </div>
   )
