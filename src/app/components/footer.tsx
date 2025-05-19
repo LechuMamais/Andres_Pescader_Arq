12cef405f87bb9a95b4dab57bb618f556d9f798d
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-05-19 14:03:46 +0200

    UPDATE: Footer colors aligned with color mode

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='text-center py-16 bg-white text-black tracking-wide font-[400]'>
+    <footer className='text-center py-16 bg-black text-white  dark:bg-white dark:text-black tracking-wide font-[400]'>
       <p className='text-xl mb-2'>© {new Date().getFullYear()} Andres Pescader Arq</p>
       <p className='text-xl'>- Developed by Alexis Mamais -</p>
     </footer>
