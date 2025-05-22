82d6743261bdf8ce16e9bf5157c92bbd23ab1321
LechuMamais <119885029+LechuMamais@users.noreply.github.com>
2025-05-22 19:05:20 +0200

    UPDATE: Refactor ProjectTitle component for improved layout and responsiveness

--- src/app/components/projectTitle.tsx
+++ src/app/components/projectTitle.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { motion } from 'framer-motion'
 import { PortableText, PortableTextBlock } from 'next-sanity'
+import { useEffect, useState } from 'react'
 
 interface ProjectTitleProps {
   titulo: string
@@ -6,19 +10,47 @@ interface ProjectTitleProps {
 }
 
 export default function ProjectTitle({ titulo, descripcion }: ProjectTitleProps) {
+  const [minHeight, setMinHeight] = useState('100vh')
+
+  useEffect(() => {
+    function updateHeight() {
+      const headerHeight = window.innerWidth >= 768 ? 192 : 128
+      setMinHeight(`calc(100vh - ${headerHeight}px)`)
+    }
+
+    updateHeight() // Ejecutar en el montaje
+
+    window.addEventListener('resize', updateHeight)
+    return () => window.removeEventListener('resize', updateHeight)
+  }, [])
+
   return (
-    <>
-      <h2
+    <section
+      className='flex flex-col justify-around items-center px-4 min-h-[calc(100vh - 8rem)] md:min-h-[calc(100vh - 12rem)]'
+      style={{ minHeight }}
+    >
+      <motion.h2
         itemProp='name'
-        className='text-3xl text-center py-12 tracking-wide font-[400] smooch-sans'
+        className='text-3xl text-center py-4 tracking-wide font-[400] smooch-sans'
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
       >
         <span className='tracking-widest'>[ </span>
         {titulo}
         <span className='tracking-widest'> ]</span>
-      </h2>
-      <h3 className='text-xl text-center py-4 tracking-wider max-w-[1080px] mx-auto px-8 lg:px-4'>
+      </motion.h2>
+
+      <motion.h3
+        className='text-xl text-center py-4 pb-24 tracking-wider max-w-[1080px] mx-auto px-8 lg:px-4'
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.2 }}
+        viewport={{ once: true }}
+      >
         <PortableText value={descripcion} />
-      </h3>
-    </>
+      </motion.h3>
+    </section>
   )
 }
